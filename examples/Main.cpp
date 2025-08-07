b0d489a4fdf444a17ccc8594e1a1b872108af89a
LebedevIlyaG <lebedev.ilya.g@gmail.com>
2025-08-07 12:48:51 +0300

    Исправлена ошибка с pi

--- examples/Main.cpp
+++ examples/Main.cpp
@@ -88,10 +88,10 @@ int main(int argc, char* argv[])
     std::vector<double>(parameters.Dimension, 1.8), // нижняя граница
     std::vector<std::function<double(const double*)>>(1, [](const double* y)
       {
-        double M_PI = 3.14159265358979323846;
+        double pi_ = 3.14159265358979323846;
         double sum = 0.;
         for (int j = 0; j < parameters.Dimension; j++)
-          sum += y[j] * y[j] - 10. * cos(2.0 * M_PI * y[j]) + 10.0;
+          sum += y[j] * y[j] - 10. * cos(2.0 * pi_ * y[j]) + 10.0;
         return sum;
       }), // критерий
     true, // определен ли оптимум
