bd19097e6e3ff0972184d4ad538df9a271f6ac75
Daniil Anufriev <danufriev2004@gmail.com>
2025-08-25 18:19:17 +0300

    init

--- tests/src/test_queue.cpp
+++ tests/src/test_queue.cpp
@@ -6,7 +6,7 @@
   класса #PriorityQueue, которая будет использоваться в тестах
  */
 
-char values [6]= {'a', 'b', 'c', 'd', 'e', 'f'};
+QueueBaseData tmp1;
 
 class TQueueTest : public ::testing::Test
 {
@@ -25,9 +25,9 @@ protected:
   }
   void SetUpFullQueue()
   {
-    queue->Push(2, 2, values + 1);
-    queue->Push(4, 4, values + 3);
-    queue->Push(6, 6, values + 5);
+    queue->Push(2, 2, &tmp1);
+    queue->Push(4, 4, &tmp1);
+    queue->Push(6, 6, &tmp1);
   }
 };
 /**
@@ -48,11 +48,11 @@ TEST_F(TQueueTest, can_create_queue_with_correct_size)
 {
   ASSERT_NO_THROW(PriorityQueue q(1023));
 }
-//
-//TEST_F(TQueueTest, throws_when_memory_for_queue_not_allocated)
-//{
-//  ASSERT_ANY_THROW(PriorityQueue q((MaxQueueSize + 1) * 2 - 1));
-//}
+
+TEST_F(TQueueTest, throws_when_memory_for_queue_not_allocated)
+{
+  ASSERT_ANY_THROW(PriorityQueue q((MaxQueueSize + 1) * 2));
+}
 
 /**
  * Проверка корректности работы метода #IsEmpty
@@ -73,116 +73,116 @@ TEST_F(TQueueTest, can_push_element)
   double globalKey = 1;
   double localKey = 1;
 
-  queue->Push(globalKey, localKey, values);
+  queue->Push(globalKey, localKey, &tmp1);
 
   ASSERT_EQ(1, queue->GetSize());
 }
 
-//TEST_F(TQueueTest, not_doing_push_to_fill_queue_when_element_is_less_then_min_key)
-//{
-//  double key;
-//  void* value;
-//  SetUpFullQueue(); //fill queue {(2, 2, "b"),(4, 4, "d"),(6, 6, "f")}
-//
-//  queue->Push(1, 1, values); //1 < 2
-//
-//  /// get element with min global key
-//  /// and check, that it is not (1, 1, "a")
-//  for (int i = 0; i < 3; i++)
-//  {
-//    queue->Pop(&key, &value);
-//  }
-//  ASSERT_NE(1, key);
-//}
+TEST_F(TQueueTest, not_doing_push_to_fill_queue_when_element_is_less_then_min_key)
+{
+  double key;
+  void* value;
+  SetUpFullQueue(); //fill queue {(2, 2, "b"),(4, 4, "d"),(6, 6, "f")}
 
-//TEST_F(TQueueTest, can_push_to_full_queue_when_element_with_largest_key)
-//{
-//  double key;
-//  void* value;
-//  SetUpFullQueue(); //fill queue {(2, 2, "b"),(4, 4, "d"),(6, 6, "f")}
-//
-//  queue->Push(7, 7, values + 3); // 7 > 6
-//
-//  /// get element with max global key
-//  /// and check, that it is (7, 7, "g")
-//  queue->Pop(&key, &value);
-//  ASSERT_EQ(7, key);
-//}
+  queue->Push(1, 1, &tmp1); //1 < 2
 
-//TEST_F(TQueueTest, can_push_to_full_queue_when_element_is_greater_then_min_key)
-//{
-//  double key;
-//  void* value;
-//  SetUpFullQueue(); //fill queue {(2, 2, "b"),(4, 4, "d"),(6, 6, "f")}
-//
-//  queue->Push(3, 3, values + 2); //3 > 2
-//
-//
-//  /// check, that element (3, 3, "c") isin queue
-//  for (int i = 0; i < maxSize; i++)
-//  {
-//    queue->Pop(&key, &value);
-//  }
-//  ASSERT_EQ(3, key);
-//}
+  /// get element with min global key
+  /// and check, that it is not (1, 1, "a")
+  for (int i = 0; i < 3; i++)
+  {
+    queue->Pop(&key, &value);
+  }
+  ASSERT_NE(1, key);
+}
 
-//TEST_F(TQueueTest, can_push_to_queue_when_element_is_less_then_min_key)
-//{
-//  double key;
-//  void* value = NULL;
-//  char* resValue = values;
-//  queue->Push(2, 2, values + 1);
-//  queue->Push(3, 3, values + 2);
-//
-//  queue->Push(1, 1, resValue);
-//
-//  for (int i = 0; i < 3; i++)
-//  {
-//    queue->Pop(&key, &value);
-//  }
-//  ASSERT_EQ(1, key);
-//  ASSERT_EQ(resValue, (char*)value);
-//}
+TEST_F(TQueueTest, can_push_to_full_queue_when_element_with_largest_key)
+{
+  double key;
+  void* value;
+  SetUpFullQueue(); //fill queue {(2, 2, "b"),(4, 4, "d"),(6, 6, "f")}
+
+  queue->Push(7, 7, &tmp1); // 7 > 6
+
+  /// get element with max global key
+  /// and check, that it is (7, 7, "g")
+  queue->Pop(&key, &value);
+  ASSERT_EQ(7, key);
+}
+
+TEST_F(TQueueTest, can_push_to_full_queue_when_element_is_greater_then_min_key)
+{
+  double key;
+  void* value;
+  SetUpFullQueue(); //fill queue {(2, 2, "b"),(4, 4, "d"),(6, 6, "f")}
+
+  queue->Push(3, 3, &tmp1); //3 > 2
+
+
+  /// check, that element (3, 3, "c") isin queue
+  for (int i = 0; i < maxSize; i++)
+  {
+    queue->Pop(&key, &value);
+  }
+  ASSERT_EQ(3, key);
+}
+
+TEST_F(TQueueTest, can_push_to_queue_when_element_is_less_then_min_key)
+{
+  double key;
+  void* value = NULL;
+  QueueBaseData resvalue;
+  queue->Push(2, 2, &tmp1);
+  queue->Push(3, 3, &tmp1);
+
+  queue->Push(1, 1, &resvalue);
+
+  for (int i = 0; i < 3; i++)
+  {
+    queue->Pop(&key, &value);
+  }
+  ASSERT_EQ(1, key);
+  ASSERT_EQ((resvalue.GetQueueElementa())->pValue, (char*)value);
+}
 
 /**
  * Проверка корректности работы метода #IsFull
  */
-//TEST_F(TQueueTest, can_detect_when_queue_is_full)
-//{
-//  SetUpFullQueue();
-//
-//  ASSERT_TRUE(queue->IsFull());
-//}
+TEST_F(TQueueTest, can_detect_when_queue_is_full)
+{
+  SetUpFullQueue();
+
+  ASSERT_TRUE(queue->IsFull());
+}
 
 TEST_F(TQueueTest, can_detect_not_full_queue_when_it_is_empty)
 {
   ASSERT_FALSE(queue->IsFull());
 }
 
-//TEST_F(TQueueTest, can_detect_when_queue_is_not_full)
-//{
-//  queue->Push(1, 1, values);
-//  queue->Push(2, 2, values + 1);
-//
-//  ASSERT_FALSE(queue->IsFull());
-//}
+TEST_F(TQueueTest, can_detect_when_queue_is_not_full)
+{
+  queue->Push(1, 1, &tmp1);
+  queue->Push(2, 2, &tmp1);
+
+  ASSERT_FALSE(queue->IsFull());
+}
 
 /**
  * Проверка корректности работы метода #Pop
  */
-//TEST_F(TQueueTest, can_pop_element)
-//{
-//  double key;
-//  void* value;
-//  char* resValue = values + 1;
-//  queue->Push(1, 1, values);
-//  queue->Push(2, 2, resValue);
-//
-//  queue->Pop(&key, &value);
-//
-//  ASSERT_EQ(2, key);
-//  ASSERT_EQ(resValue, (char*)value);
-//}
+TEST_F(TQueueTest, can_pop_element)
+{
+  double key;
+  void* value;
+ QueueBaseData resvalue;
+  queue->Push(1, 1, &tmp1);
+  queue->Push(2, 2, &resvalue);
+
+  queue->Pop(&key, &value);
+
+  ASSERT_EQ(2, key);
+  ASSERT_EQ((resvalue.GetQueueElementa())->pValue, (char*)value);
+}
 
 TEST_F(TQueueTest, throws_when_pop_from_empty_queue)
 {
@@ -198,83 +198,83 @@ TEST_F(TQueueTest, throws_when_pop_from_empty_queue)
  */
 TEST_F(TQueueTest, can_push_to_empty_queue)
 {
-  queue->PushWithPriority(1, 1, values);
+  queue->PushWithPriority(1, 1, &tmp1);
 
   ASSERT_FALSE(queue->IsEmpty());
 }
 
-//TEST_F(TQueueTest, can_push_when_element_is_greater_then_min_key)
-//{
-//  double key;
-//  void* value;
-//  queue->PushWithPriority(1, 1, values);
-//  queue->PushWithPriority(3, 3, values + 2);
-//
-//  queue->PushWithPriority(2, 2, values + 2);
-//
-//  queue->Pop(&key, &value);
-//  queue->Pop(&key, &value);
-//  ASSERT_EQ(2, key);
-//}
-//
-//TEST_F(TQueueTest, can_push_when_element_is_equal_to_min_key)
-//{
-//  double key;
-//  void* value = NULL;
-//  char* resValue = values + 2;
-//  queue->PushWithPriority(1, 1, values);
-//  queue->PushWithPriority(2, 2, values + 1);
-//
-//  queue->PushWithPriority(1, 1, resValue);
-//
-//  for (int i = 0; i < 3; i++)
-//  {
-//    queue->Pop(&key, &value);
-//  }
-//
-//  ASSERT_EQ(1, key);
-//  ASSERT_EQ(resValue, (char*)value);
-//}
+TEST_F(TQueueTest, can_push_when_element_is_greater_then_min_key)
+{
+  double key;
+  void* value;
+  queue->PushWithPriority(1, 1, &tmp1);
+  queue->PushWithPriority(3, 3, &tmp1);
 
-//TEST_F(TQueueTest, can_push_with_priority_to_full_queue_when_element_is_greater_then_min_key)
-//{
-//  double key;
-//  void* value;
-//  char* resValue = values + 2;
-//  SetUpFullQueue(); //{(2, 2, "b"),(4, 4, "d"),(6, 6, "f")}
-//
-//  queue->PushWithPriority(3, 3, resValue);
-//
-//  queue->Pop(&key, &value);
-//  queue->Pop(&key, &value);
-//  queue->Pop(&key, &value);
-//
-//  ASSERT_EQ(3, key);
-//  ASSERT_EQ(resValue, (char*)value);
-//}
-//
-//TEST_F(TQueueTest, not_doing_push_to_queue_when_element_is_less_then_min_key)
-//{
-//  double key;
-//  void* value;
-//  char* resValue = values;
-//  queue->PushWithPriority(2, 2, values + 1);
-//  queue->PushWithPriority(3, 3, values + 2);
-//
-//  queue->PushWithPriority(1, 1, resValue);
-//
-//  queue->Pop(&key, &value);
-//  queue->Pop(&key, &value);
-//  ASSERT_TRUE(queue->IsEmpty());
-//}
+  queue->PushWithPriority(2, 2, &tmp1);
+
+  queue->Pop(&key, &value);
+  queue->Pop(&key, &value);
+  ASSERT_EQ(2, key);
+}
+
+TEST_F(TQueueTest, can_push_when_element_is_equal_to_min_key)
+{
+  double key;
+  void* value = NULL;
+  QueueBaseData resvalue;
+  queue->PushWithPriority(1, 1, &tmp1);
+  queue->PushWithPriority(2, 2, &tmp1);
+
+  queue->PushWithPriority(1, 1, &resvalue);
+
+  for (int i = 0; i < 3; i++)
+  {
+    queue->Pop(&key, &value);
+  }
+
+  ASSERT_EQ(1, key);
+  ASSERT_EQ((resvalue.GetQueueElementa())->pValue, (char*)value);
+}
+
+TEST_F(TQueueTest, can_push_with_priority_to_full_queue_when_element_is_greater_then_min_key)
+{
+  double key;
+  void* value;
+  QueueBaseData resvalue;
+  SetUpFullQueue(); //{(2, 2, "b"),(4, 4, "d"),(6, 6, "f")}
+
+  queue->PushWithPriority(3, 3, &resvalue);
+
+  queue->Pop(&key, &value);
+  queue->Pop(&key, &value);
+  queue->Pop(&key, &value);
+
+  ASSERT_EQ(3, key);
+  ASSERT_EQ((resvalue.GetQueueElementa())->pValue, (char*)value);
+}
+
+TEST_F(TQueueTest, not_doing_push_to_queue_when_element_is_less_then_min_key)
+{
+  double key;
+  void* value;
+  QueueBaseData resvalue;
+  queue->PushWithPriority(2, 2, &tmp1);
+  queue->PushWithPriority(3, 3, &tmp1);
+
+  queue->PushWithPriority(1, 1, &resvalue);
+
+  queue->Pop(&key, &value);
+  queue->Pop(&key, &value);
+  ASSERT_TRUE(queue->IsEmpty());
+}
 /**
  * Проверка корректности работы метода #Clear
  */
-//TEST_F(TQueueTest, can_clear_queue)
-//{
-//  SetUpFullQueue();
-//
-//  queue->Clear();
-//
-//  ASSERT_TRUE(queue->IsEmpty());
-//}
+TEST_F(TQueueTest, can_clear_queue)
+{
+  SetUpFullQueue();
+
+  queue->Clear();
+
+  ASSERT_TRUE(queue->IsEmpty());
+}
