51c9d62d4796e6b0c9dd908075a626f4f5a772e6
LebedevIlyaG <lebedev.ilya.g@gmail.com>
2025-09-16 18:13:49 +0300

    Поправлен пример

--- examples/Main.cpp
+++ examples/Main.cpp
@@ -135,33 +135,33 @@ int main(int argc, char* argv[])
   // Решаем задачу
   if (solver.Solve() != SYSTEM_OK)
     throw EXCEPTION("Error: solver.Solve crash!!!");
-#elif
-  {
-    parameters.Dimension = 2;
+#else
 
-    auto problem = new ProblemFromFunctionPointers(parameters.Dimension, // размерность задачи
-      std::vector<double>(parameters.Dimension, -2.2), // верхняя граница
-      std::vector<double>(parameters.Dimension, 1.8), // нижняя граница
-      std::vector<std::function<double(const double*)>>(1, [](const double* y)
-        {
-          double pi_ = 3.14159265358979323846;
-          double sum = 0.;
-          for (int j = 0; j < parameters.Dimension; j++)
-            sum += y[j] * y[j] - 10. * cos(2.0 * pi_ * y[j]) + 10.0;
-          return sum;
-        }), // критерий
-      true, // определен ли оптимум
-      0, // значение глобального оптимума
-      std::vector<double>(parameters.Dimension, 0).data() // координаты глобального минимума
+  parameters.Dimension = 2;
 
-    );
-    problem->Initialize();
-    // Решатель
-    Solver solver(problem);
-    // Решаем задачу
-    if (solver.Solve() != SYSTEM_OK)
-      throw EXCEPTION("Error: solver.Solve crash!!!");
-  }
+  auto problem = new ProblemFromFunctionPointers(parameters.Dimension, // размерность задачи
+    std::vector<double>(parameters.Dimension, -2.2), // верхняя граница
+    std::vector<double>(parameters.Dimension, 1.8), // нижняя граница
+    std::vector<std::function<double(const double*)>>(1, [](const double* y)
+      {
+        double pi_ = 3.14159265358979323846;
+        double sum = 0.;
+        for (int j = 0; j < parameters.Dimension; j++)
+          sum += y[j] * y[j] - 10. * cos(2.0 * pi_ * y[j]) + 10.0;
+        return sum;
+      }), // критерий
+    true, // определен ли оптимум
+    0, // значение глобального оптимума
+    std::vector<double>(parameters.Dimension, 0).data() // координаты глобального минимума
+
+  );
+  problem->Initialize();
+  // Решатель
+  Solver solver(problem);
+  // Решаем задачу
+  if (solver.Solve() != SYSTEM_OK)
+    throw EXCEPTION("Error: solver.Solve crash!!!");
+
 #endif
 
 
