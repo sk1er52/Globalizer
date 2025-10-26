f7eb4c78b44b9b2ca0205a503c9b1b15befaf1e0
LebedevIlyaG <lebedev.ilya.g@gmail.com>
2025-10-26 21:06:50 +0300

    Исправлен вывод

--- globalizer/method/src/Method.cpp
+++ globalizer/method/src/Method.cpp
@@ -806,7 +806,7 @@ void Method::InsertPoints(const std::vector<Trial*>& points)
     Trial* currentPoint = points[j];
     Extended  x;
 
-    for (int j = 0; j < currentPoint->index; j++)
+    for (int j = 0; j <= currentPoint->index; j++)
     {
       functionCalculationCount[j] += 1;
     }
