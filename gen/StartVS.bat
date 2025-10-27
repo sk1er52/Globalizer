1daba04be0258cd0eca795a2b5d625f99d4288d1
LebedevIlyaG <lebedev.ilya.g@gmail.com>
2025-10-27 17:00:02 +0300

    Исправлен путь до окружения

--- gen/StartVS.bat
+++ gen/StartVS.bat
@@ -16,18 +16,18 @@ git submodule update
 call conda init
 
 echo [1/5] Creating a Conda Environment...
-call conda create -p "%ROOT_DIR%build_64\Globalizer_env" python=3.11 -y
+call conda create -p "%ROOT_DIR%\build_64\Globalizer_env" python=3.11 -y
 
 echo [2/5] activate a Conda Environment...
-call conda activate "%ROOT_DIR%build_64\Globalizer_env"
+call conda activate "%ROOT_DIR%\build_64\Globalizer_env"
 
 echo [3/5] Installing the library...
-call conda install -p "%ROOT_DIR%build_64\Globalizer_env" numpy -y
-call conda install -p "%ROOT_DIR%build_64\Globalizer_env" jsonschema -y
-call conda install -p "%ROOT_DIR%build_64\Globalizer_env" pytorch::pytorch -y
-call conda install -p "%ROOT_DIR%build_64\Globalizer_env" conda-forge::pytorch-lightning -y
-call conda install -p "%ROOT_DIR%build_64\Globalizer_env" lightning -y
-call conda install -p "%ROOT_DIR%build_64\Globalizer_env" scikit-learn -y
+call conda install -p "%ROOT_DIR%\build_64\Globalizer_env" numpy -y
+call conda install -p "%ROOT_DIR%\build_64\Globalizer_env" jsonschema -y
+call conda install -p "%ROOT_DIR%\build_64\Globalizer_env" pytorch::pytorch -y
+call conda install -p "%ROOT_DIR%\build_64\Globalizer_env" conda-forge::pytorch-lightning -y
+call conda install -p "%ROOT_DIR%\build_64\Globalizer_env" lightning -y
+call conda install -p "%ROOT_DIR%\build_64\Globalizer_env" scikit-learn -y
 
 echo [4/5] CMake Configuration...
 call cmake -G "Visual Studio 17 2022" -DGLOBALIZER_BUILD_PROBLEMS=ON -DGLOBALIZER_BUILD_GCGEN=ON -DBUILD_ALL_TASK=ON -DGLOBALIZER_MAX_DIMENSION=130 -DGLOBALIZER_MAX_Number_Of_Function=70 -DGLOBALIZER_BUILD_TESTS=ON ..
